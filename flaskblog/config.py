c75d3549e8095aacec6053efc2aaf237ada185e5
rich2965 <47729414+rich2965@users.noreply.github.com>
2021-10-15 14:54:55 -0500

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,10 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-    #SECRET_KEY = 'd1977121aa0c44c245afcb47d5268264'
+    #SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SECRET_KEY = 'd1977121aa0c44c245afcb47d5268264'
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:10141992@localhost/flaskwebapp'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
