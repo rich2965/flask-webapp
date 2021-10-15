ddb7c2a089648b8034293facdb9fe1a0d4b74297
rich2965 <47729414+rich2965@users.noreply.github.com>
2021-10-15 15:56:00 -0500

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -6,7 +6,7 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:10141992@localhost/flaskwebapp'
-    DATABASE_URL = 'postgres://ymomattahygpyd:4d4740cb9cf8531e41f0d2fd38b00839a7d5844d191865329ede40c7a74ae1e6@ec2-18-214-214-252.compute-1.amazonaws.com:5432/d6hgutmgbd82ag'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ymomattahygpyd:4d4740cb9cf8531e41f0d2fd38b00839a7d5844d191865329ede40c7a74ae1e6@ec2-18-214-214-252.compute-1.amazonaws.com:5432/d6hgutmgbd82ag'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
